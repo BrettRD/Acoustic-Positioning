ad411884876fde47816e37fac78d2e81115c3338
BrettRD <brett.downing1@gmail.com>
2017-07-01 20:41:56 +0800

    running the convolution in a loop on recorded audio

--- simulation.m
+++ simulation.m
@@ -1,25 +1,44 @@
 
 
+more off;
+
 
 #attenuation = 100;
 ref1 = audioread ("ReferenceNoise1.wav");
-ref2 = audioread ("ReferenceNoise2.wav");
-ref3 = audioread ("ReferenceNoise3.wav");
-ref4 = audioread ("ReferenceNoise4.wav");
-ref5 = audioread ("ReferenceNoise5.wav");
-noise = audioread ("BackgroundRealLoop1.wav");
-
+#ref2 = audioread ("ReferenceNoise2.wav");
+#ref3 = audioread ("ReferenceNoise3.wav");
+#ref4 = audioread ("ReferenceNoise4.wav");
+#ref5 = audioread ("ReferenceNoise5.wav");
+#noise = audioread ("BackgroundRealLoop1.wav");
 
 
 #micSim = [];
-#for i=1:10;
 #	micSim = cat (1, micSim, ref1 + (0.3 * ref2) +  (0.01*ref3) + ref4);
-#endfor
+while 1;
+
+	printf("selecting mic\n")
+	microphone = audiorecorder(44100, 16, 1, 0);
+
+	printf("starting record\n");
+	recordblocking(microphone,5);
+
+	printf("collecting data\n");
+	soundSample = getaudiodata(microphone);
+	croppedaudio = soundSample(44100:end);	#drop the first second
+
+	#size(soundSample)
+	#size(croppedaudio)
 
 
-conv1 = fftconv(noise, flip(ref1));
 
-#conv2 = fftconv(noise, flip(ref2));
+	printf("processing convolution\n")
+	conv1 = fftconv(croppedaudio, flip(ref1));
 
-plot (1:size(conv1)(1), conv1);
+	#conv2 = fftconv(noise, flip(ref2));
 
+	printf("plotting\n")
+	plot (1:size(conv1)(1), conv1);
+	drawnow;
+
+
+endwhile
